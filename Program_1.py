1363ca84d6a00f02a86f8545d1dd911a94687c59
rajat799 <rajatmalapur@gmail.com>
2025-04-17 22:28:29 +0530

    array properties

--- Program_1.py
+++ Program_1.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-arr1 = np.array([10, 20, 30])
+arr1 = np.array([10, 20, 30, 49, 50])
 arr2 = np.array([[1, 2], [3, 4]])
 
 print("1D Array: ", arr1)
 print("2D Array: \n", arr2)
+
+# array properties
+print("Shape: ", arr1.shape)  # to check how many elements are there in array
+print("Datatype: ", arr1.dtype)  # used to check the datatype of the array elements
+print("size: ", arr2.size)  # to check the size of the array (n)
+
+
 
