f00bb8e423d1c1d203a0f644854fb0cca9304101
rajat799 <rajatmalapur@gmail.com>
2025-04-17 23:00:16 +0530

    array operations

--- Program_1.py
+++ Program_1.py
@@ -11,5 +11,11 @@ print("Shape: ", arr1.shape)  # to check how many elements are there in array
 print("Datatype: ", arr1.dtype)  # used to check the datatype of the array elements
 print("size: ", arr2.size)  # to check the size of the array (n)
 
+# array operations
+print("add 5 to arr1: ", arr1 + 5)  # add 5 in all elements of the array
+print("multiply arr1 by 2: ", arr1 * 2)  # multiply 2 by all elements in array
 
+arr3 = np.array([10, 15, 20, 25])
+arr4 = np.array([1, 1, 1, 1])
 
+print("Element wise addition: ", arr3 + arr4)
